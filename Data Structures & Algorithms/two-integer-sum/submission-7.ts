fee28f9b62a1771a4c8823b35c53d539cdafa088
Peter <huynhxpeter@gmail.com>
2026-07-17 23:03:23 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        // iterate nums 
+        // i + j = target
+        let numsSeenMap = {}
+        for (let i=0; i < nums.length; i++) {
+            const currNumb = nums[i]
+            // check if difference exists in map by calc
+            const difference = target - currNumb
+            if (numsSeenMap[difference] !== undefined) {
+                return [i, numsSeenMap[difference]]
+            }
+            numsSeenMap[currNumb] = i
+            
+        }
+    }
+}
